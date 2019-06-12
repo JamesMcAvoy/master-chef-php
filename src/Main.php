942ee2f5c09fd7f9b3fc09704b585894896770ae
JamesMcAvoy <mathieu.brisset@laposte.net>
2019-06-12 18:46:08 +0200

    server

---
+++ src/Main.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Resto;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Main implements MessageComponentInterface {
+    protected $client;
+
+    public function __construct() {
+        $this->client = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->client->attach($conn);
+
+        echo "Client connected\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+
+        echo sprintf('Client %d sending message "%s"' . "\n"
+            , $from->resourceId
+            , $msg);
+
+        //$client->send($msg);
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->client->detach($conn);
+
+        echo "Client has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error has occurred: {$e->getMessage()}\n";
+
+        $conn->close();
+    }
+}
